e5762a73900c547f2be30be4aeca68bdb6fcb7fc
Wairice <126349455+Wairice@users.noreply.github.com>
2023-12-09 12:03:51 +0100

    Description hello.c

--- Lab №3/hello.c
+++ Lab №3/hello.c
@@ -5,48 +5,53 @@
 #include <linux/slab.h>
 #include <linux/ktime.h>
 
-MODULE_AUTHOR ("Stanislav Osipov <stasosipov09092004@gmail.com>");
-MODULE_DESCRIPTION ("Hello, this is LabWork №3");
+MODULE_AUTHOR("Stanislav Osipov <stasosipov09092004@gmail.com>");
+MODULE_DESCRIPTION("Hello, this is LabWork №3");
 MODULE_LICENSE("Dual BSD/GPL");
 
-static struct list_head hello_list_head;
-
+// Структура для подій, яка містить list_head і ktime_t
 struct hello_event {
     struct list_head list;
     ktime_t time;
 };
 
-static uint count = 1;
+// Голова списку
+static struct list_head hello_list_head;
+
+// Параметр для визначення кількості разів друкування "Hello, world!"
+static uint count = 1; // Параметр типу uint з 1 за замовчуванням
 module_param(count, uint, S_IRUGO);
 MODULE_PARM_DESC(count, "Count of times to print \"Hello, world!\"");
 
+// Функція ініціалізації модуля
 static int __init hello_init(void)
 {
     int i;
     struct hello_event *ptr;
 
+    // Перевірка на валідність значення count
     if (count == 0) {
-        pr_warn("Count is 0.\n");
+        pr_warn("Count is 0.\n"); // Якщо значення параметра 0, друкується попередження, але робота продовжу'ється.
     }
-    
+
     if (count >= 5 && count <= 10) {
-        pr_warn("Count is between 5 and 10.\n");
+        pr_warn("Count is between 5 and 10.\n"); // Якщо значення параметра знаходиться між 5 і 10, друкується попередження, але робота продовжу'ється.
     }
 
     if (count > 10) {
-        pr_err("Count is greater than 10. \nExiting with error.\n");
+        pr_err("Count is greater than 10. \nExiting with error.\n"); // Якщо значення більше 10, друкується повідомлення про помилку і повернутається значення -EINVAL
         return -EINVAL;
     }
 
-    INIT_LIST_HEAD(&hello_list_head);
+    INIT_LIST_HEAD(&hello_list_head); // Ініціалізація голови списку
 
     for (i = 0; i < count; ++i) {
-        ptr = kmalloc(sizeof(*ptr), GFP_KERNEL);
+        ptr = kmalloc(sizeof(*ptr), GFP_KERNEL); // Виділення пам'яті для структури
         if (!ptr)
             return -ENOMEM;
 
-        ptr->time = ktime_get();
-        list_add_tail(&ptr->list, &hello_list_head);
+        ptr->time = ktime_get(); // Запис поточного часу в структуру
+        list_add_tail(&ptr->list, &hello_list_head); // Додавання структури до списку
 
         pr_emerg("Hello, world!\n");
     }
@@ -54,14 +59,16 @@ static int __init hello_init(void)
     return 0;
 }
 
+// Функція видалення модуля
 static void __exit hello_exit(void)
 {
     struct hello_event *ptr, *temp;
 
+    // Прохід по списку
     list_for_each_entry_safe(ptr, temp, &hello_list_head, list) {
-        pr_info("Time: %lld ns\n", ktime_to_ns(ptr->time));
-        list_del(&ptr->list);
-        kfree(ptr);
+        pr_info("Time: %lld ns\n", ktime_to_ns(ptr->time)); // Друк часу події в наносекундах
+        list_del(&ptr->list); // Вилучення елемента зі списку
+        kfree(ptr); // Звільнення пам'яті
     }
 }
 
